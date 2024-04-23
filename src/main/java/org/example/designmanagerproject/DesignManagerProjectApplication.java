5ca15fefabd7c7d84efbd6925c2c029783d8306b
duy <dangnhatduy98@gmail.com>
2024-04-23 15:45:04 +0700

    init project

---
+++ src/main/java/org/example/designmanagerproject/DesignManagerProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.designmanagerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignManagerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignManagerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/designmanagerproject/DesignManagerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.designmanagerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignManagerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
